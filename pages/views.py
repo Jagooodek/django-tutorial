aca56ce73d594b3a4d737853b3a7bb9399239ca9
Paweł Wojciechowski <pawelwkdp@gmail.com>
2022-05-06 21:38:28 +0200

    add home, about and base page with tests

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 from django.http import HttpResponse
 
-def homePageView(request):
-    return HttpResponse('Hello, World!')
-# Create your views here.
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
